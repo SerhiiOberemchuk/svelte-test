fa872ee7d7317151e0ba9ace4907b30ead54fa6a
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-01-20 22:25:10 +0100

    base commit install svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let page = 'about';
+</script>
+
+<div class=" flex flex-col items-center justify-center">
+	<h1 class=" bg-slate-600">{page}</h1>
+	<p>prosto text</p>
+	<a href="/">home page</a>
+</div>
