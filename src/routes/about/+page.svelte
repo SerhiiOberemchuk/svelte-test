d56a8e4bd9962044a611fdef85d1ebf0fd2e937e
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-01-21 18:09:43 +0100

    add animation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,19 @@
 <script>
 	export let page = 'about';
+	import { Tween, Spring } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+	import { fade, slide } from 'svelte/transition';
+	let progress = new Tween(0, { duration: 1000, easing: cubicOut });
 </script>
 
-<div class=" flex flex-col items-center justify-center">
+<div class=" flex flex-col items-center justify-center gap-5">
 	<h1 class=" bg-slate-600">{page}</h1>
-	<p>prosto text</p>
+
 	<a href="/">home page</a>
+	<div in:fade out:slide>Привіт, світ!</div>
+	{#each [{ id: 1, text: 'Анімація!' }] as item (item.id)}
+		<div>{item.text}</div>
+	{/each}
+	<div in:fade={{ duration: 1000 }}>Повільна поява</div>
+	<div style="width: {progress}%;">Прогрес</div>
 </div>
